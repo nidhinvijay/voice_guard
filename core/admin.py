0e21b73bc1a361affb9295ba97a402195765e6be
nidhinvijay <nidhinvijaynv@gmail.com>
2025-09-19 15:01:23 +0530

    Models

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import FilteredPhrase
-
-@admin.register(FilteredPhrase)
-class FilteredPhraseAdmin(admin.ModelAdmin):
-    list_display = ('offensive_phrase', 'language', 'updated_at')
-    list_filter = ('language',)
-    search_fields = ('offensive_phrase',)
