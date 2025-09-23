63a8c73e80530cf90ed6f8cdbd75310cbc41ebef
nidhinvijay <nidhinvijaynv@gmail.com>
2025-09-23 14:36:27 +0530

    database region

--- core/static/core/js/firebase-init.js
+++ core/static/core/js/firebase-init.js
@@ -1,9 +1,10 @@
+// core/static/core/js/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Uses the firebaseConfig object created in the HTML
-const app = initializeApp(firebaseConfig);
+// Uses the firebaseConfig object that is now safely attached to the window
+const app = initializeApp(window.firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
--- core/views.py
+++ core/views.py
@@ -40,6 +40,6 @@ def get_firebase_config():
         "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
         "projectId": os.getenv("FIREBASE_PROJECT_ID"),
         "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-        "messagingSenderId": os.getenv("FIREBEASE_MESSAGING_SENDER_ID"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
         "appId": os.getenv("FIREBASE_APP_ID"),
     }
